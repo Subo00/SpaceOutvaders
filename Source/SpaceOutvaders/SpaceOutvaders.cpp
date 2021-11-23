e06e8b987fa4f8cc032f7d29252158c5a85e2b0b
Ernest Subotić <93708543+Subo00@users.noreply.github.com>
2021-11-24 00:18:15 +0100

    Initial commit

---
+++ Source/SpaceOutvaders/SpaceOutvaders.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SpaceOutvaders.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceOutvaders, "SpaceOutvaders" );
